2e408a69252d856313731f544f1d9cbb4184a151
pradeeppratihast <pradeep.pratihast016@gmail.com>
2024-04-15 20:59:06 +0530

    Update js file

--- 01-colorChanger/script.js
+++ 01-colorChanger/script.js
@@ -1,1 +1,8 @@
+const body = document.querySelector("body");
+const buttonList = document.querySelectorAll(".button");
 
+buttonList.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    body.style.backgroundColor = e.target.id;
+  });
+});
